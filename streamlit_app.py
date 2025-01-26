6986d82f481288d52d5261d7330e8ea55397be25
Likitha2525 <likitha.buggana2525@gmail.com>
2025-01-26 23:12:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+from snowflake.snowspark.functions import col
 
 # Write directly to the app
 st.title("customize your smoothie :smoothie:")
